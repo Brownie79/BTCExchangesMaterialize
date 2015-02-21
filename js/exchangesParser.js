54fea167fa818b9a93dcbe3fb5cdb9614f70f6d4
Brownie79 <1wolfer0@gmail.com>
2015-02-20 21:01:48 -0600

    Parser still not works, but js loading properly now

--- js/exchangesParser.js
+++ js/exchangesParser.js
@@ -1,31 +1,26 @@
-$(document).ready(function() { //Does funtion() when file loads
-	//alert("Test1"); //works
-
-});
-
-
-
-
-/*
 $(document).ready(function() {
+	document.getElementById("label").innerText = "Test John"; //works
 	var file = new XMLHttpRequest();
     file.open("GET", "exchanges.txt", false);	//may need to put actual file location
     file.onreadystatechange = function () {
 		var obj = JSON.parse(this);				//check if "this" works
-		document.getElementById("lablel").innerText = obj[0].name;		//may need .innerHTML
+		document.getElementById("label").innerText = obj[0].name;		//may need .innerHTML
 	};
 });
-*/
+
 
 /*
 
-var request = new XMLHttpRequest();
-request.open("GET","exchanges.txt",false);
-request.onreadystatechange = function() {
-	if(request.status===200 && request.readyState---4){
-		var words = request.responseText.split(' ');
-		alert(words[0]);
+$(document).ready(function() { //Does funtion() when file loads
+	alert("Test1"); //works
+	var request = new XMLHttpRequest();
+	request.open("GET","exchanges.txt",false);
+	request.onreadystatechange = function() {
+		if(request.status===200 && request.readyState---4){
+			var words = request.responseText.split(' ');
+			alert(words[0]);
+		}
 	}
-}
+});
 
 */
