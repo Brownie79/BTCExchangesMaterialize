c02bbf729e24acf58c21905b7551daf038aa17d9
Brownie79 <1wolfer0@gmail.com>
2015-02-20 20:44:56 -0600

    Testing using alert for parser
    Somewhere something does not work

--- js/exchangesParser.js
+++ js/exchangesParser.js
@@ -1,3 +1,11 @@
+$(document).ready(function() { //Does funtion() when file loads
+	alert("Test1");
+});
+
+
+
+
+/*
 $(document).ready(function() {
 	var file = new XMLHttpRequest();
     file.open("GET", "exchanges.txt", false);	//may need to put actual file location
@@ -6,3 +14,17 @@ $(document).ready(function() {
 		document.getElementById("lablel").innerText = obj[0].name;		//may need .innerHTML
 	};
 });
+*/
+
+/*
+
+var request = new XMLHttpRequest();
+request.open("GET","exchanges.txt",false);
+request.onreadystatechange = function() {
+	if(request.status===200 && request.readyState---4){
+		var words = request.responseText.split(' ');
+		alert(words[0]);
+	}
+}
+
+*/
