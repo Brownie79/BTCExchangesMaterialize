005281ce38d20aaa979dfb5b23b761fedbb81ba5
Brownie79 <1wolfer0@gmail.com>
2015-02-24 10:53:25 -0600

    Parse() splits up data into line arrays

--- js/exchangesParser.js
+++ js/exchangesParser.js
@@ -2,12 +2,34 @@
 $.get( "exchanges.txt")
 .done(function( data ) {
     $( ".result" ).html( data );
-    alert("checked it");
-    document.getElementById('label').innerText = data;
+    alert("checked it"); //works
+    parse(data); //call works
 })
 .fail(function() {
     alert( "error" );
 })
 .always(function() {
-    alert( "finished" );
+    alert( "finished"); //annoying so switched it off //left method for reference
 });
+
+function parse(data){
+  var array = data.split('\n'); //array of lines from exchagnes.txt
+  alert(array.length);
+
+
+
+  //alert(obj[0]);  works
+  //document.getElementById('label').innerText = data;
+}
+
+
+/*
+$(document).ready(function() {
+	var file = new XMLHttpRequest();
+    file.open("GET", "exchanges.txt", false);	//may need to put actual file location
+    file.onreadystatechange = function () {
+		var obj = JSON.parse(this);				//check if "this" works
+		document.getElementById("lablel").innerText = obj[0].name;		//may need .innerHTML
+	};
+});
+*/
