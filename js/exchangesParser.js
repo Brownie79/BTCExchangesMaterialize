87b7b98e56a341cb2589126e0573da656d2d5c41
Brownie79 <1wolfer0@gmail.com>
2015-02-24 00:03:43 -0600

    Updated Parser.js
    Moving dir after this to htdocs folder to run local server

--- js/exchangesParser.js
+++ js/exchangesParser.js
@@ -1,3 +1,83 @@
+//Attempt 7
+$.get( "exchanges.txt")
+.done(function( data ) {
+    $( ".result" ).html( data );
+    alert("checked it");
+})
+.fail(function() {
+    alert( "error" );
+})
+.always(function() {
+    alert( "finished" );
+});
+
+
+
+
+/*
+//Attempt 6
+alert("Test Alert 9"); //js file does load into index.html
+$.get( "exchanges.txt",
+ 	function( data ) {
+		//idk what the following two lines do, I got them from:
+		//http://api.jquery.com/jquery.get/
+  $( ".result" ).html( data );
+  alert("check it");
+	})
+	//should execute if works?
+	.done(function() {
+		alert( "second success" );
+	})
+	//should execute if any error
+	.fail(function(response) {
+		alert( "error" );
+		console.log("my response is",response)
+	})
+	.always(function() {
+		alert( "finished" );
+	});
+*/
+
+/*
+// Attempt 5
+alert("Test Alert");
+var jqxhr = $.get( "exchanges.txt", function() {
+  alert( "success" );
+	})
+  .done(function() {
+    alert( "second success" );
+  })
+  .fail(function() {
+    alert( "error" );
+  })
+  .always(function() {
+    alert( "finished" );
+  });
+	// Perform other work here ...
+	// Set another completion function for the request above
+	jqxhr.always(function() {
+  	alert( "second finished" );
+	}
+
+);
+*/
+
+/* I tried
+$(document).ready(function()
+	//var reader = new FileReader();
+	readText();
+	function readText(){
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			var output = e.target.result;
+			document.getElementById('label').innerHTML = output;
+			reader.readAsText('exchanges.txt')
+		}
+	}
+)};
+*/
+
+/*
 $(document).ready(function() {
 	document.getElementById("label").innerText = "Test John"; //works
 	var file = new XMLHttpRequest();
@@ -7,10 +87,8 @@ $(document).ready(function() {
 		document.getElementById("label").innerText = obj[0].name;		//may need .innerHTML
 	};
 });
-
-
+*/
 /*
-
 $(document).ready(function() { //Does funtion() when file loads
 	alert("Test1"); //works
 	var request = new XMLHttpRequest();
@@ -22,5 +100,4 @@ $(document).ready(function() { //Does funtion() when file loads
 		}
 	}
 });
-
 */
