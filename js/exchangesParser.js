7d0c34e60f991bdeacc331a0bcf0677f00662f30
Brownie79 <1wolfer0@gmail.com>
2015-02-24 19:12:31 -0600

    Got the Cards made and show up
    Cards don't really work with the reveal property

--- js/exchangesParser.js
+++ js/exchangesParser.js
@@ -2,7 +2,7 @@
 $.get( "exchanges.txt")
 .done(function( data ) {
     $( ".result" ).html( data );
-    alert("checked it"); //works
+    alert("file fetched!"); //prints if file fetched
     parse(data); //call works
 })
 .fail(function() {
@@ -12,6 +12,46 @@ $.get( "exchanges.txt")
     alert( "finished"); //prints when parser is finished, if no print, then parse didn't execute properly
 });
 
+
+function makeCard(name,link,image,loc,type,crypto,fiat,desc){
+  var div = "";
+  div = '<div class="card card-division"> <!-- CARD -->'+
+          '<div class="card-image waves-effect waves-block waves-light">'+
+            '<img class="activator responsive-img" src="'+image+'">'+
+          '</div>'+
+          '<div class="card-content">'+
+            '<span class="card-title activator grey-text text-darken-4">'+
+            name + '<i class="mdi-navigation-more-vert right"></i>' +
+            '</span> <p>'+link+'</p>'+
+          '</div>'+
+          '<div class="card-reveal">'+
+            '<span class="card-title grey-text text-darken-4">'+
+            name+' <i class="mdi-navigation-close right"></i>'+
+            '</span>'+
+            '<p> Location:'+loc+'</p>'+
+            '<p> Type:'+type+'</p>'+
+            '<p> Crypto Currencies:'+crypto+'</p>'+
+            '<p> Fiat Currencies:'+fiat+'</p>'+
+            '<p> Descripton:'+desc+'</p>'+
+          '</div>'+
+        '</div> <!--Card End-->';
+  return div;
+}
+
+
+function addToHTML(divArray){
+  var div = "";
+  for(i = 0; i<divArray.length;i++){
+    div = div + divArray[i]; //make one giant string of all the cards
+  }
+  document.getElementById("cards").innerHTML = div;
+  //following works:
+  //div = "<div>Suppity sup</div>";
+  //document.getElementById("cards").innerHTML = div;
+  //alert('blah');
+}
+
+
 function parse(data){
   var array = data.split('\n'); //array of lines from exchanges.txt
   //need to trim the array for the \n char
@@ -20,7 +60,7 @@ function parse(data){
       array[i] = array[i].trim();
       //alert('"' + array[i] + '"'); //after trim
   }
-
+  var divArray = []; //array of text file.
   var i = 0;
   while( i < array.length){ //while array is one less than max length
     if(array[i] == "--EXCHANGE--"){
@@ -33,26 +73,29 @@ function parse(data){
       var fiat   = array[i+7].split("|")[1];
       var desc   = array[i+8].split("|")[1];
       i = i + 9;
-      alert(name+"\n"+link+"\n"+image+"\n"+loc+"\n"+type+"\n"+crypto+"\n"+fiat+"\n"+desc);
+      //alert(name+"\n"+link+"\n"+image+"\n"+loc+"\n"+type+"\n"+crypto+"\n"+fiat+"\n"+desc);
+      var card = makeCard(name,link,image,loc,type,crypto,fiat,desc);
+      divArray[divArray.length] = card; //adds a new card at the end of the array
+      alert(divArray[divArray.length]);
     } else {
-      i++; //to stop from getting stuck in an infinite loop
+      i++; //protect against INF loop
     }
-  }
-
-
-  /*
-  NEED to trim each line for the \n char
-  alert('"' + array[i].trim() + '"'); //prints --EXCHANGE--
-  if(array[i].trim()=="--EXCHANGE--"){
-    alert("true");
-  } else {
-    alert("false");
-  }
-  */
-  //alert(array.length);
-  //alert(array[0]);  //--Exchange--
-  //alert(array[27]); //blank space !! <-- END OF FILE
-  //alert(array[28]); //undefined
-  //alert(obj[0]);  works
-  //document.getElementById('label').innerText = data;
+  } //end while
+  addToHTML(divArray);
 }
+
+/*
+NEED to trim each line for the \n char
+alert('"' + array[i].trim() + '"'); //prints --EXCHANGE--
+if(array[i].trim()=="--EXCHANGE--"){
+  alert("true");
+} else {
+  alert("false");
+}
+*/
+//alert(array.length);
+//alert(array[0]);  //--Exchange--
+//alert(array[27]); //blank space !! <-- END OF FILE
+//alert(array[28]); //undefined
+//alert(obj[0]);  works
+//document.getElementById('label').innerText = data;
